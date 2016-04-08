b453ba40c0c19b9d5fc458ebf68b1bb7b5238e64
nmolif00 <nmolif00@estudiantes.unileon.es>
2016-04-08 21:26:25 +0200

    Actualizado 08.04 21:26

--- Clase-Date-master/src/es/unileon/prg1/MainDate.java
+++ Clase-Date-master/src/es/unileon/prg1/MainDate.java
@@ -1,3 +1,7 @@
+package es.unileon.prg1.date;
+
+
+
 
 public class MainDate {
 
@@ -5,16 +9,23 @@ public class MainDate {
 public static void main (String [] args) {
 
 	try {
-		Date firstDate=new Date (17,10,1996);
-		
-
-		System.out.println("El mes que ha introducido es" + firstDate.monthName(firstDate));
-		System.out.println("La estación que ha introducido es" + firstDate.getSeason(firstDate));
-		System.out.println("El día del mes es" +firstDate.monthRight(firstDate));
+		Date date1=new Date (17,10,1996);
+		Date date2=new Date (1,1,2016);
 
 
-	}catch (DateException exception) {
-		System.err.println(exception.toString());
+
+		System.out.println("El mes que ha introducido es" + date1.monthName(date1));
+		System.out.println("El mes que ha introducido es" + date2.monthName(date2));
+
+		System.out.println("La estación que ha introducido es" + date1.getSeason(date1));
+		System.out.println("La estación que ha introducido es" + date2.getSeason(date2));
+
+		System.out.println("El día del mes es" +date1.monthRight(date1));
+		System.out.println("El día del mes es" +date2.monthRight(date2));
+
+
+	}catch (DateException Exception) {
+		System.err.println(Exception.toString());
 	}
 
    }
